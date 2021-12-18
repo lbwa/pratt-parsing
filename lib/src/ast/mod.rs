67b256b612f4496bac87a361c6b2c5cbc34b7d83
Liu Bowen <mr_lbw@outlook.com>
2021-12-18 23:36:51 +0800

    feat(parser): declare expression statement

--- lib/src/ast/mod.rs
+++ lib/src/ast/mod.rs
@@ -1,5 +1,11 @@
+mod expr;
 mod stmt;
 
+pub use self::expr::*;
 pub use self::stmt::*;
+
+/// It represents a kind of AST node, unlike `crate::token::Token::Ident(String)` which represents a kind of Token type.
+#[derive(PartialEq, Debug, Clone)]
+pub struct Ident(pub String);
 
 pub type Program = Vec<Statement>;
--- lib/src/ast/stmt.rs
+++ lib/src/ast/stmt.rs
@@ -1,8 +1,18 @@
-#[derive(PartialEq, Debug, Clone)]
-pub struct Ident(pub String);
+use super::expr;
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum Statement {
-  Let(Ident),
+  Let(super::Ident, expr::Expr),
   Return,
+  /// It's a statement that consists solely of one expression. and not really
+  /// a distinct statement, and only a wrapper.
+  /// We need it because it's totally legal in Monkey to write the following
+  /// code:
+  /// ```
+  /// let x = 5; // normal statement
+  /// x + 10; // expression statement
+  /// ```
+  /// As we see, Expr that is a kind variant of Statement, which means we can
+  /// add it to the Statements slice of ast::Program
+  Expr(expr::Expr),
 }
