93ab4c99f7b26a3e6e718549b7001155d10f450c
Liu Bowen <mr_lbw@outlook.com>
2022-01-04 23:58:30 +0800

    feat(interpreter): eval prefix expr

--- interpreter/src/eval/mod.rs
+++ interpreter/src/eval/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod test;
 
+use std::borrow::Borrow;
+
 use super::object::Object;
 use monkey_rust::ast::{self, Expr};
 
@@ -12,10 +14,17 @@ impl Evaluator {
     Evaluator {}
   }
 
+  pub fn error(message: String) -> Object {
+    Object::Error(message)
+  }
+
   pub fn eval(&self, stmts: Vec<ast::Statement<'_>>) -> Option<Object> {
     let mut result: Option<Object> = None;
     for stmt in stmts {
-      result = self.eval_stmt(stmt);
+      match self.eval_stmt(stmt) {
+        Some(Object::Error(message)) => return Some(Object::Error(message)),
+        object => result = object,
+      }
     }
     result
   }
@@ -30,6 +39,7 @@ impl Evaluator {
   fn eval_expr(&self, expr: ast::Expr) -> Option<Object> {
     match expr {
       Expr::Literal(literal) => self.eval_literal(literal),
+      Expr::Prefix(prefix, expr) => self.eval_prefix_expr(prefix, expr),
       _ => None,
     }
   }
@@ -38,6 +48,45 @@ impl Evaluator {
     match literal {
       ast::Literal::Int(val) => Some(Object::Int(val)),
       ast::Literal::Bool(val) => Some(Object::Bool(val)),
+    }
+  }
+}
+
+impl Evaluator {
+  fn eval_prefix_expr(&self, prefix: ast::Prefix, expr: Box<ast::Expr>) -> Option<Object> {
+    let expr = (expr.borrow() as &ast::Expr).clone();
+    if let Some(result) = self.eval_expr(expr) {
+      match prefix {
+        ast::Prefix::Bang => {
+          let result = match result {
+            Object::Bool(false) => Object::Bool(true),
+            Object::Int(val) => {
+              if val == 0 {
+                Object::Bool(true) // !0 should be treated as false
+              } else {
+                Object::Bool(false)
+              }
+            }
+            _ => Object::Bool(false),
+          };
+          Some(result)
+        }
+
+        ast::Prefix::Minus => {
+          let result = match result {
+            Object::Int(val) => Object::Int(-val),
+            _ => Self::error(format!("Illegal syntax: -{}", result)),
+          };
+          Some(result)
+        }
+
+        ast::Prefix::Plus => match result {
+          Object::Int(_) => Some(result),
+          _ => Some(Self::error(format!("Illegal syntax: +{}", result))),
+        },
+      }
+    } else {
+      None
     }
   }
 }
--- interpreter/src/eval/test.rs
+++ interpreter/src/eval/test.rs
@@ -28,3 +28,40 @@ fn eval_bool_expr() {
     assert_eq!(eval!(input), Some(Object::Bool(expected)))
   }
 }
+
+#[test]
+fn eval_bang_operator() {
+  let cases = vec![
+    ("!true", false),
+    ("!false", true),
+    ("!!true", true),
+    ("!!!true", false),
+    ("!!false", false),
+    ("!!!false", true),
+    ("!0", true),
+    ("!!0", false),
+    ("!!-1", true),
+  ];
+
+  for (input, expected) in cases {
+    assert_eq!(eval!(input), Some(Object::Bool(expected)))
+  }
+}
+
+#[test]
+fn eval_prefix_minus_operator() {
+  let cases = vec![("-1", -1), ("-10", -10)];
+
+  for (input, expected) in cases {
+    assert_eq!(eval!(input), Some(Object::Int(expected)))
+  }
+}
+
+#[test]
+fn eval_prefix_plus_operator() {
+  let cases = vec![("+0", 0), ("+1", 1)];
+
+  for (input, expected) in cases {
+    assert_eq!(eval!(input), Some(Object::Int(expected)))
+  }
+}
