5f1073b36a1cd4394abf9cb6bf8e4da47774a355
Liu Bowen <mr_lbw@outlook.com>
2022-01-04 23:58:30 +0800

    feat(interpreter): eval prefix expr

--- interpreter/src/object.rs
+++ interpreter/src/object.rs
@@ -4,14 +4,16 @@ use std::fmt;
 pub enum Object {
   Int(i64),
   Bool(bool),
+
+  Error(String),
 }
 
 impl fmt::Display for Object {
   fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-    let val = match *self {
-      Object::Int(val) => val.to_string(),
-      Object::Bool(val) => val.to_string(),
-    };
-    write!(formatter, "{}", val)
+    match *self {
+      Object::Int(ref val) => write!(formatter, "{}", val),
+      Object::Bool(val) => write!(formatter, "{}", val),
+      Object::Error(ref val) => write!(formatter, "{}", val),
+    }
   }
 }
