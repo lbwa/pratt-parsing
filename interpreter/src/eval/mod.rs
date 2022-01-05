d336edc300ea9a2ad9de1f188f453e770fda0010
Liu Bowen <mr_lbw@outlook.com>
2022-01-05 23:26:32 +0800

    feat(interpreter): eval infix expr

--- interpreter/src/eval/mod.rs
+++ interpreter/src/eval/mod.rs
@@ -1,8 +1,6 @@
 #[cfg(test)]
 mod test;
 
-use std::borrow::Borrow;
-
 use super::object::Object;
 use pratt_parsing::ast::{self, Expr};
 
@@ -40,6 +38,7 @@ impl Evaluator {
     match expr {
       Expr::Literal(literal) => self.eval_literal(literal),
       Expr::Prefix(prefix, expr) => self.eval_prefix_expr(prefix, expr),
+      Expr::Infix(left, infix, right) => self.eval_infix_expr(*left, infix, *right),
       _ => None,
     }
   }
@@ -55,8 +54,7 @@ impl Evaluator {
 // eval_x_expr
 impl Evaluator {
   fn eval_prefix_expr(&self, prefix: ast::Prefix, expr: Box<ast::Expr>) -> Option<Object> {
-    let expr = (expr.borrow() as &ast::Expr).clone();
-    self.eval_expr(expr).map(|result| {
+    self.eval_expr(*expr).map(|result| {
       match prefix {
         ast::Prefix::Bang => {
           let result = match result {
@@ -87,5 +85,30 @@ impl Evaluator {
         },
       }
     })
+  }
+
+  fn eval_infix_expr(
+    &self,
+    left_expr: ast::Expr,
+    infix: ast::Infix,
+    right_expr: ast::Expr,
+  ) -> Option<Object> {
+    let left = self.eval_expr(left_expr)?;
+    let right = self.eval_expr(right_expr)?;
+
+    use ast::Infix;
+    let result = match infix {
+      Infix::Equal => Object::Bool(left == right),
+      Infix::NotEqual => Object::Bool(left != right),
+      Infix::LessThan => Object::Bool(left < right),
+      Infix::LessThanEqual => Object::Bool(left <= right),
+      Infix::GreaterThan => Object::Bool(left > right),
+      Infix::GreaterThanEqual => Object::Bool(left >= right),
+      Infix::Plus => left + right,
+      Infix::Minus => left - right,
+      Infix::Multiply => left * right,
+      Infix::Divide => left / right,
+    };
+    Some(result)
   }
 }
--- interpreter/src/object.rs
+++ interpreter/src/object.rs
@@ -1,6 +1,7 @@
 use std::fmt;
+use std::ops::{Add, Div, Mul, Sub};
 
-#[derive(PartialEq, Clone, Debug)]
+#[derive(PartialEq, Clone, Debug, PartialOrd)]
 pub enum Object {
   Int(i64),
   Bool(bool),
@@ -15,5 +16,54 @@ impl fmt::Display for Object {
       Object::Bool(val) => write!(formatter, "{}", val),
       Object::Error(ref val) => write!(formatter, "{}", val),
     }
+  }
+}
+
+impl Add for Object {
+  type Output = Object;
+  fn add(self, rhs: Object) -> Object {
+    if let Object::Int(left_val) = self {
+      if let Object::Int(right_val) = rhs {
+        return Object::Int(left_val + right_val);
+      }
+    }
+    // TODO: handle the addition for string type
+    Object::Error(format!("Couldn't perform operation: {} + {}", self, rhs,))
+  }
+}
+
+impl Sub for Object {
+  type Output = Object;
+  fn sub(self, rhs: Object) -> Object {
+    if let Object::Int(left_val) = self {
+      if let Object::Int(right_val) = rhs {
+        return Object::Int(left_val - right_val);
+      }
+    }
+    Object::Error(format!("Couldn't perform operation: {} - {}", self, rhs,))
+  }
+}
+
+impl Mul for Object {
+  type Output = Self;
+  fn mul(self, rhs: Self) -> Self {
+    if let Object::Int(left_val) = self {
+      if let Object::Int(right_val) = rhs {
+        return Object::Int(left_val * right_val);
+      }
+    }
+    Object::Error(format!("Couldn't perform operation: {} * {}", self, rhs,))
+  }
+}
+
+impl Div for Object {
+  type Output = Self;
+  fn div(self, rhs: Self) -> Self {
+    if let Object::Int(left_val) = self {
+      if let Object::Int(right_val) = rhs {
+        return Object::Int(left_val / right_val);
+      }
+    }
+    Object::Error(format!("Couldn't perform operation: {} / {}", self, rhs,))
   }
 }
